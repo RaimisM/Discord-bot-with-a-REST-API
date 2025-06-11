24c5aafb95cf45891f46ff1c36f51294d9a1dfe5
RaimisM <raimondasmicc@gmail.com>
2025-06-11 17:02:37 +0300

    fix(getRandomTemplate): update template repository import and method for fetching templates

--- src/modules/messages/utils/getRandomTemplate.ts
+++ src/modules/messages/utils/getRandomTemplate.ts
@@ -1,9 +1,9 @@
-import createTemplatesRepository from '@/modules/templates/repository'
+import { createTemplatesRepository } from '@/modules/templates/repository'
 import { Database } from '@/database'
 
 export default async function getRandomTemplate(db: Database) {
   const repo = createTemplatesRepository(db)
-  const templates = await repo.getAllTemplates()
+  const templates = await repo.findAll()
 
   if (!templates.length) {
     throw new Error('No templates available')
